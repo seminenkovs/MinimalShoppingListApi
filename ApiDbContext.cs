919615ec5093487fd932db3a7cf23c2a3aa15f9a
Vitalii <seminenkovs@gmail.com>
2023-07-30 01:22:55 +0200

    add project to GitHub

---
+++ ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalShoppingListApi
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Grocery> Groceries => Set<Grocery>();
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
